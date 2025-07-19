b3b5d426b41fe76503cf33eddcea5417b29f2a18
PavitranBhat <161752419+PavitranBhat@users.noreply.github.com>
2025-07-19 10:48:28 +0530

    Add files via upload

---
+++ Array/Traversal.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int arr[5] = {10, 20, 30, 40, 50};
+    printf("Enter the array elements:");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d", arr[i]);
+    }
+}
