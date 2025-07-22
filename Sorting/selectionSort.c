407697cfccdd9137d15a951c31db03d76c0b90e2
Pavitra Bhat <161752419+PavitranBhat@users.noreply.github.com>
2025-07-22 18:53:20 +0530

    Add files via upload

---
+++ Sorting/selectionSort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx, temp;
+
+    // Move the boundary of unsorted array one by one
+    for (i = 0; i < n - 1; i++) {
+        // Find the minimum element in unsorted part
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+
+        // Swap the found minimum element with the first element
+        if (min_idx != i) {
+            temp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp;
+        }
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
