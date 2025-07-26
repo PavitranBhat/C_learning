dbd13afbbb2304b64a90bdff191782fdb310316c
Pavitra Bhat <161752419+PavitranBhat@users.noreply.github.com>
2025-07-26 14:16:14 +0530

    Add files via upload

---
+++ Array/KadaneAlgorithm.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for(int i=0; i<n; i++) scanf("%d", &a[i]);
+
+    int max_sum = a[0], curr_sum = a[0];
+    for(int i=1; i<n; i++){
+        if(curr_sum < 0) curr_sum = a[i];
+        else curr_sum += a[i];
+        if(curr_sum > max_sum) max_sum = curr_sum;
+    }
+    printf("Max Subarray Sum: %d\n", max_sum);
+    return 0;
+}
